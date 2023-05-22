1f1b2c44198450e6a550b5877597e8e039e28892
MarcoGalantino <65068632+MarcoGalantino@users.noreply.github.com>
2023-05-22 16:27:29 +0200

    Creazione account finita al 50%

--- lib/login.dart
+++ lib/login.dart
@@ -12,13 +12,6 @@ class InitAmministratoreHomePage extends StatefulWidget {
 class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> {
   @override
   Widget build(BuildContext context) {
-    int width = MediaQuery.of(context).size.width.toInt();
-    int height = MediaQuery.of(context).size.height.toInt();
-    double devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
-    String aspectRatio =
-        MediaQuery.of(context).size.aspectRatio.toStringAsFixed(4);
-    Orientation orientation = MediaQuery.of(context).orientation;
-
     return const Scaffold(
       // drawer: const Drawer(),
       // appBar: AppBar(title: Text(widget.title), actions: <Widget>[
@@ -49,6 +42,7 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
               child: Padding(
                   padding: EdgeInsets.all(32),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -56,10 +50,13 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
                         Text("Benvenuto su Ratatouille23", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black87),),
                       ],
                     ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[Text("Inserisci le seguenti informazioni per inizializzare il sistema!", style: TextStyle(fontSize: 16, color: Colors.white60),)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[Text("Inserisci le seguenti informazioni per inizializzare il sistema!", style: TextStyle(fontSize: 16, color: Colors.white60),)
                   ],
+                ),Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+
                 ),
                   ],
                 ),
