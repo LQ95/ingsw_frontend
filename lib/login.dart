86d2483987c6490acabbb8c70cac0e1ab0de9d88
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-05-23 18:12:58 +0200

    commento

--- lib/login.dart
+++ lib/login.dart
@@ -19,7 +19,7 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
   //Ma
   void sendData(String name, String pass) async {
     var apiUrl=Uri.http('localhost:8080','/api/v1/utente'); //URL del punto di contatto della API
-    var response = await http.post(apiUrl,
+    var response = await http.post(apiUrl, //questa è la response,in cui è definita anche la request, direttamente
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
