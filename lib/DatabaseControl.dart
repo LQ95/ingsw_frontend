6916dfdc4961bd8de00a36051201d3df9d2f1f6b
MarcoGalantino <65068632+MarcoGalantino@users.noreply.github.com>
2023-05-24 16:47:39 +0200

    Gestione risposte asincrone di DatabaseControl e visualizzazione delle notifiche

--- lib/DatabaseControl.dart
+++ lib/DatabaseControl.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class DatabaseControl {
-  void sendData(String name, String pass, String ruolo) async {
+  Future<String> sendData(String name, String pass, String ruolo) async {
     var apiUrl = Uri.http('localhost:8080',
         '/api/v1/utente'); //URL del punto di contatto della API
     var response = await http.post(apiUrl,
@@ -15,7 +15,12 @@ class DatabaseControl {
           'ruolo': ruolo})
     );
 
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    if(response.statusCode.toInt() == 200) {
+      return "SUCCESSO";
+    } else {
+      return "FALLIMENTO";
+    }
+    // print('Response status: ${response.statusCode}');
+    // print('Response body: ${response.body}');
   }
 }
--- lib/InitAmministratoreHomePage.dart
+++ lib/InitAmministratoreHomePage.dart
@@ -21,8 +21,8 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
   @override
   Widget build(BuildContext context) {
     //ritrovano il testo immesso nei textfield
-    final controller1= TextEditingController();
-    final controller2= TextEditingController();
+    final controller1 = TextEditingController();
+    final controller2 = TextEditingController();
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -53,75 +53,104 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
                 borderRadius: BorderRadius.all(Radius.circular(25)),
               ),
               child: Padding(
-                  padding: const EdgeInsets.all(32),
+                padding: const EdgeInsets.all(32),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[Center(child:
-                        Expanded(child: Text("Benvenuto su Ratatouille23", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black87), maxLines: 2, overflow: TextOverflow.ellipsis,),)
+                      Expanded(child: Text("Benvenuto su Ratatouille23",
+                        style: TextStyle(fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,),)
                       ),
-                        ],
+                      ],
                     ),
                     const Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[ Expanded(child: Text("Inserisci le seguenti informazioni per inizializzare il sistema!", style: TextStyle(fontSize: 16, color: Colors.white60), overflow: TextOverflow.ellipsis, maxLines: 3,))
-                  ],
-                ),
+                      children: <Widget>[
+                        Expanded(child: Text(
+                          "Inserisci le seguenti informazioni per inizializzare il sistema!",
+                          style: TextStyle(fontSize: 16, color: Colors.white60),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,))
+                      ],
+                    ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Expanded( child:
+                        children: [Expanded(child:
                         Padding(
                           padding: const EdgeInsets.only(left: 64, right: 64,),
-                          child:TextField(
+                          child: TextField(
                             controller: controller1,
-                            decoration:  const InputDecoration(
+                            decoration: const InputDecoration(
                               border: OutlineInputBorder(),
                               labelText: 'Nome Account:',
                             ),
-                          ),),)]
+                          ),),)
+                        ]
                     ),
-                     Row(
+                    Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Expanded( child:
-                          Padding(
+                        children: [Expanded(child:
+                        Padding(
                           padding: const EdgeInsets.only(left: 64, right: 64,),
-                          child:TextField(
-                              controller: controller2,
-                              obscureText: true,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Password:',
+                          child: TextField(
+                            controller: controller2,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password:',
                             ),
-                          ),),)]
+                          ),),)
+                        ]
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        ElevatedButton(onPressed: (){
-                          if(controller1.text.isNotEmpty && controller2.text.isNotEmpty) {
-                            DatabaseControl db = DatabaseControl(); db.sendData(controller1.text,controller2.text, "AMMINISTRATORE");
+                        ElevatedButton(onPressed: () async {
+                          if (controller1.text.isNotEmpty && controller2.text
+                              .isNotEmpty) {
+                            DatabaseControl db = DatabaseControl();
+                            String creazioneAvvenutaConSuccesso = await db.sendData(controller1.text, controller2.text, "AMMINISTRATORE");  //Il client attende la risposta del server prima di proseguire, in modo che
+                            if (creazioneAvvenutaConSuccesso == "SUCCESSO") {                                                               //il valore di ritorno di tipo Future ottenga uno stato
+                              showAllertSuccesso();
+                            } else if (creazioneAvvenutaConSuccesso == "FALLIMENTO"){
+                                showAllertErrore("Il nome che hai selezionato è già stato scelto...");
+                            } else {
+                              showAllertErrore("Si è verificato un errore inaspettato, per favore riprovare...");
                             }
-                          else {
-                            showAllert();
                           }
-                          },
+                          else {
+                            showAllertErrore(
+                                "Attenzione, i campi non sono stati compilati correttamente!");
+                          }
+                        },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF66420F),
-                            ),
-                          child: const Text("Conferma", style: TextStyle(color: Colors.white70),),
+                          ),
+                          child: const Text("Conferma", style: TextStyle(
+                              color: Colors.white70),),
                         )
                       ],
                     ),
                     const Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[ Expanded(child: Text("Attenzione, l'account così creato sarà quello di amministratore del sistema e non potrà essere modificato successivamente", style: TextStyle(fontSize: 16, color: Color(0xFFA52A70)), overflow: TextOverflow.ellipsis, maxLines: 3,))
+                      children: <Widget>[
+                        Expanded(child: Text(
+                          "Attenzione, l'account così creato sarà quello di amministratore del sistema e non potrà essere modificato successivamente",
+                          style: TextStyle(fontSize: 16,
+                              color: Color(0xFFA52A70)),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,))
                       ],
                     ),
                   ],
                 ),
-                  )
+              )
           ),
         ),
       ),
@@ -129,11 +158,20 @@ class InitAmministratoreHomePageState extends State<InitAmministratoreHomePage> 
   }
 
 
-  void showAllert() {
+  void showAllertErrore(String errore) {
     QuickAlert.show(context: context,
-    type: QuickAlertType.error,
-    text: "Attenzione, i campi non sono stati compilati correttamente!",
-    title: "Errore"
+        type: QuickAlertType.error,
+        text: errore,
+        title: "Qualcosa è andato storto"
+    );
+  }
+
+
+  void showAllertSuccesso() {
+    QuickAlert.show(context: context,
+        type: QuickAlertType.success,
+        text: "Eccellente, l'account è stato creato con successo!",
+        title: "Successo!"
     );
   }
 }
