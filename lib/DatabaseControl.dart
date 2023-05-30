982e7020637233172c040e687de0b2ac56284068
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-05-30 11:03:28 +0200

    aggiungo baseUrl

--- lib/DatabaseControl.dart
+++ lib/DatabaseControl.dart
@@ -2,8 +2,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class DatabaseControl {
+  final String baseUrl = '192.168.1.3:8080'; //Ip Marco  192.168.1.138:8080
   Future<String> sendUserData(String name, String pass, String ruolo) async {
-    var apiUrl = Uri.http('192.168.1.138:8080', //Ip Marco  192.168.1.138:8080
+    var apiUrl = Uri.http(baseUrl,
         '/api/v1/utente'); //URL del punto di contatto della API
     var response = await http.post(apiUrl,
         //questa è la response,in cui è definita anche la request, direttamente
@@ -35,7 +36,7 @@ class DatabaseControl {
       'password': pass,
     };
 
-    var apiUrl = Uri.http('192.168.1.138:8080',
+    var apiUrl = Uri.http(baseUrl,
         '/api/v1/utente/auth',loginParameters); //URL del punto di contatto della API,più udsername e pass come parametri
     var response = await http.get(apiUrl,
         //questa è la response,in cui è definita anche la request, direttamente
