964fe7c21a97bf2a6fc6894596335b69690dad96
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-06-28 12:07:13 +0200

    correggo errori

--- lib/control/OrdinazioneControl.dart
+++ lib/control/OrdinazioneControl.dart
@@ -3,27 +3,27 @@ import 'dart:convert';
 import '../GlobImport.dart';
 import 'package:http/http.dart' as http;
 
-class OrdinazioneControl{
+class OrdinazioneControl {
+
   Future<List?> getCurrentOrdinazione(int tavolo) async {
-  var apiUrl=Uri.http(baseUrl,"api/v1/ordinazione/getcurrent",{"tavoloId":tavolo});
-  var response=await http.get(apiUrl);
-  List<dynamic> ordinazione;
+    var apiUrl = Uri.http(baseUrl, "api/v1/ordinazione/getcurrent", {"tavoloId": tavolo.toString()});
+    var response = await http.get(apiUrl);
+    List<dynamic> ordinazione;
 
-  if (response.statusCode == 200) {
-    ordinazione= jsonDecode(utf8.decode(response.bodyBytes));
-    print(ordinazione);
-    return ordinazione;
-  }
-  else {
-    return null;
-  }
-
-
+    if (response.statusCode == 200) {
+      ordinazione = jsonDecode(utf8.decode(response.bodyBytes));
+      print(ordinazione);
+      return ordinazione;
+    }
+    else {
+      return null;
+    }
   }
 
   Future<bool> closeCurrentOrdinazione(int tavolo) async {
-    var apiUrl=Uri.http(baseUrl,"api/v1/ordinazione/closecurrent",{"tavoloId":tavolo});
-    var response=await http.get(apiUrl);
+    var apiUrl = Uri.http(
+        baseUrl, "api/v1/ordinazione/closecurrent", {"tavoloId": tavolo.toString()});
+    var response = await http.get(apiUrl);
     List<dynamic> ordinazione;
 
     if (response.statusCode == 200) {
@@ -32,5 +32,5 @@ class OrdinazioneControl{
     else {
       return false;
     }
-
+  }
 }
