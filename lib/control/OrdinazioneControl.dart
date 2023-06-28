d5f4ad24ce342c82e3666d8ff438aeafe1b858dc
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-06-28 12:01:07 +0200

    aggiungo metodo per chiudere la ordinazione

--- lib/control/OrdinazioneControl.dart
+++ lib/control/OrdinazioneControl.dart
@@ -11,13 +11,26 @@ class OrdinazioneControl{
 
   if (response.statusCode == 200) {
     ordinazione= jsonDecode(utf8.decode(response.bodyBytes));
-    // print(jsonResponse);
+    print(ordinazione);
     return ordinazione;
   }
   else {
     return null;
   }
 
+
   }
+
+  Future<bool> closeCurrentOrdinazione(int tavolo) async {
+    var apiUrl=Uri.http(baseUrl,"api/v1/ordinazione/closecurrent",{"tavoloId":tavolo});
+    var response=await http.get(apiUrl);
+    List<dynamic> ordinazione;
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    else {
+      return false;
+    }
 
 }
