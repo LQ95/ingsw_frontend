8279d313926603383743850105c8a2baecf12e2b
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-06-28 11:40:26 +0200

    Update OrdinazioneControl.dart

--- lib/control/OrdinazioneControl.dart
+++ lib/control/OrdinazioneControl.dart
@@ -1,6 +1,23 @@
+import 'dart:convert';
+
+import '../GlobImport.dart';
+import 'package:http/http.dart' as http;
+
 class OrdinazioneControl{
-  // List<dynamic> getCurrentOrdinazione(Long tavolo){
-  //
-  // }
+  Future<List?> getCurrentOrdinazione(int tavolo) async {
+  var apiUrl=Uri.http(baseUrl,"api/v1/ordinazione/getcurrent",{"tavoloId":tavolo});
+  var response=await http.get(apiUrl);
+  List<dynamic> ordinazione;
+
+  if (response.statusCode == 200) {
+    ordinazione= jsonDecode(utf8.decode(response.bodyBytes));
+    // print(jsonResponse);
+    return ordinazione;
+  }
+  else {
+    return null;
+  }
+
+  }
 
 }
