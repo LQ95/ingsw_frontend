679d79e795e0e2b7a9c120b58ac5d78206a10953
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-06-30 13:40:58 +0200

    aggiungo salvataggio a file e una dependency per metterlo nella cartella corrente

--- lib/control/PdfControl.dart
+++ lib/control/PdfControl.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pdfWidgets;
 
 class PdfControl{
 
-  static createPdfConto(Map<String,dynamic> pietanze){
+  static createPdfConto(Map<String,dynamic> pietanze) async {
   final pdf= pdfWidgets.Document();
   pdf.addPage(pdfWidgets.Page(
     pageFormat: PdfPageFormat.a4,
@@ -11,6 +14,9 @@ class PdfControl{
       //TODO effettivamente scrivere il codice della pagina
     }
   ));
+
+  final output = await getTemporaryDirectory();
+  final file = File("${output.path}/conto.pdf");
   }
 
 }
