29246223c4323796c44614575f8d93dafa33f27a
LQ95 <38260780+LQ95@users.noreply.github.com>
2023-07-01 10:22:21 +0200

    finito codice per salvare in PDF

--- lib/control/PdfControl.dart
+++ lib/control/PdfControl.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
@@ -6,7 +7,7 @@ import 'package:pdf/widgets.dart' as pdfWidgets;
 
 class PdfControl{
 
-  static createPdfConto(Map<String,dynamic> pietanze, int tavoloId) async {
+  static createPdfConto(List<dynamic> pietanze, int tavoloId,double conto) async {
   final pdf= pdfWidgets.Document();
   pdf.addPage(pdfWidgets.Page(
     pageFormat: PdfPageFormat.a4,
@@ -15,10 +16,25 @@ class PdfControl{
         child: pdfWidgets.Column(
           children:[
             pdfWidgets.Text('Conto Tavolo n°$tavoloId',style: const pdfWidgets.TextStyle(fontSize: 40)),
+            pdfWidgets.ListView.builder(itemCount: pietanze.length,itemBuilder: (context,index){
+              return pdfWidgets.Row(
+                children: [
+                  pdfWidgets.Expanded(
+                      child:pdfWidgets.Text(pietanze[index]['name'],style: const pdfWidgets.TextStyle(fontSize: 14))
+                  ),
+                  pdfWidgets.Text(pietanze[index]['costo'] + "€",style: const pdfWidgets.TextStyle(fontSize: 14))
+                ]
+              );
+            }),
+          pdfWidgets.Row(
+              children: [
+                pdfWidgets.Text("$conto€",style: const pdfWidgets.TextStyle(fontSize: 14)),
+              ]
+          )
           ]
         )
       );
-      //TODO effettivamente scrivere il codice della pagina
+
     }
   ));
 
