1936077a1638c9a451c7e1d5174b4d9fce48f947
MarcoGalantino <65068632+MarcoGalantino@users.noreply.github.com>
2023-07-04 18:36:29 +0200

    Le stats si aggiornano con le richieste della data

--- lib/SchermataStatistiche.dart
+++ lib/SchermataStatistiche.dart
@@ -2,42 +2,52 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ingsw_frontend/SchermataCreazioneAccount.dart';
 import 'package:ingsw_frontend/control/StatisticheControl.dart';
+import 'package:intl/intl.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
 import 'GlobImport.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-
+import 'package:intl/intl.dart';
 import 'entity/OrdinazioneData.dart';
 
 class SchermataStatistiche extends StatefulWidget {
-  final ValueNotifier<OrdinazioneData?> selectedDataNotifier = ValueNotifier<OrdinazioneData?>(null);
+  final ValueNotifier<OrdinazioneData?> selectedDataNotifier =
+  ValueNotifier<OrdinazioneData?>(null);
+
   @override
   _SchermataStatisticheState createState() => _SchermataStatisticheState();
 }
 
 class _SchermataStatisticheState extends State<SchermataStatistiche> {
-  List<OrdinazioneData> data = []; // Dichiarazione della variabile data
+  List<OrdinazioneData> ordinazioni = [];
+  DateTime? dataInizio;
+  DateTime? dataFine;
+
+  _SchermataStatisticheState() {
+    setDefault().then((ordinazioni) {
+      setState(() {
+        this.ordinazioni = ordinazioni;
+      });
+    });
+  }
 
 
-  Future<Widget> generaGrafico() async {
-    StatisticheControl db = StatisticheControl();
-    List<OrdinazioneData> data = await db.getGuadagniTotaliFromDB();
+  Future<Widget> generaGrafico(List<OrdinazioneData> data) async {
 
-    // Crea la serie di dati per il grafico
     charts.Series<OrdinazioneData, DateTime> series =
     charts.Series<OrdinazioneData, DateTime>(
       id: 'Ordinazioni',
       domainFn: (OrdinazioneData data, _) => data.date,
       measureFn: (OrdinazioneData data, _) => data.price,
       data: data,
-      // Configura l'etichetta del punto del grafico
-      labelAccessorFn: (OrdinazioneData data, _) => '${data.date.day}/${data.date.month}/${data.date.year}\n${data.price.toStringAsFixed(2)}',
+      labelAccessorFn: (OrdinazioneData data, _) =>
+      '${data.date.day}/${data.date.month}/${data.date.year}\n${data.price.toStringAsFixed(2)}',
     );
 
-    // Crea il grafico a linee
     charts.TimeSeriesChart chart = charts.TimeSeriesChart(
       [series],
       animate: true,
       dateTimeFactory: const charts.LocalDateTimeFactory(),
-      // Configura l'interazione del grafico per mostrare le etichette dei punti
       behaviors: [
         charts.LinePointHighlighter(
           symbolRenderer: charts.CircleSymbolRenderer(),
@@ -67,63 +77,192 @@ class _SchermataStatisticheState extends State<SchermataStatistiche> {
     );
 
     return SizedBox(
-      height: 300, // Imposta l'altezza del grafico
+      height: 300,
       child: chart,
     );
   }
 
+
   @override
   Widget build(BuildContext context) {
-    int width = MediaQuery
-        .of(context)
-        .size
-        .width
-        .toInt();
-    int height = MediaQuery
-        .of(context)
-        .size
-        .height
-        .toInt();
+    return Builder(
+      builder: (BuildContext context) {
 
-    return Scaffold(
-      appBar: GlobalAppBar,
-      drawer: buildDrawer(context),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // ...
-
-          FutureBuilder(
-            future: generaGrafico(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              } else {
-                return GestureDetector(
-                  onTap: () {
-                    widget.selectedDataNotifier.value = null;
+        return Scaffold(
+          appBar: GlobalAppBar,
+          drawer: buildDrawer(context),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 18, top: 9, right: 18),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
                   },
-                  child: snapshot.data!,
-                );
-              }
-            },
-          ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF66420F),
+                  ),
+                  child: const Text(
+                    "Indietro",
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FutureBuilder(
+                    future: generaGrafico(ordinazioni),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasError) {
+                        return Text(snapshot.error.toString());
+                      } else {
+                        return GestureDetector(
+                          onTap: () {
+                            widget.selectedDataNotifier.value = null;
+                          },
+                          child: snapshot.data!,
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+              Center(
+                child: ValueListenableBuilder<OrdinazioneData?>(
+                  valueListenable: widget.selectedDataNotifier,
+                  builder: (context, selectedData, _) {
+                    if (selectedData == null) {
+                      return const SizedBox.shrink();
+                    }
+                    return Text('Guadagni del giorno: $selectedData');
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text("Data iniziale:"),
 
-          ValueListenableBuilder<OrdinazioneData?>(
-            valueListenable: widget.selectedDataNotifier,
-            builder: (context, selectedData, _) {
-              if (selectedData == null) {
-                return const SizedBox
-                    .shrink(); // Nasconde il testo quando nessun punto è stato selezionato
-              }
-              return Text('Guadagni del giorno: $selectedData');
-            },
+                        IconButton(
+                          icon: const Icon(Icons.calendar_today),
+                          onPressed: () => selezionaDataInizio(context),
+                        ),
+                        if (dataInizio != null)
+                          Text(dataInizio!.toIso8601String()),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text("Data finale:  "),
+
+                        IconButton(
+                          icon: const Icon(Icons.calendar_today),
+                          onPressed: () => selezionaDataFine(context),
+                        ),
+                        if (dataFine != null)
+                          Text(dataFine!.toIso8601String()),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                if(dataInizio == null || dataFine == null){
+                                  showAlertErrore("Inserisci l'intervallo di date in vuoi eseguire la ricerca!");
+                                } else{
+                                  StatisticheControl db = StatisticheControl();
+                                  ordinazioni = await db.getGuadagniInDateFromDB(dataInizio!, dataFine!);
+                                  print(ordinazioni);
+                                  setState(() {});
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF66420F),
+                              ),
+                              child: const Text("Conferma", style: TextStyle(color: Colors.white70))
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                ordinazioni = await setDefault();
+                                dataFine = null;
+                                dataInizio = null;
+                                widget.selectedDataNotifier.value = null;
+                                setState(() {});
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF66420F),
+                              ),
+                              child: const Text("Reseta", style: TextStyle(color: Colors.white70))
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
+
+  Future<void> selezionaDataInizio(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: dataInizio ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null && picked != dataInizio) {
+      setState(() {
+        dataInizio = picked;
+      });
+    }
+  }
+
+  Future<void> selezionaDataFine(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: dataFine ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null && picked != dataFine) {
+      setState(() {
+        dataFine = picked;
+      });
+    }
+  }
+
+  void showAlertErrore(String errore) {
+    QuickAlert.show(context: context,
+        type: QuickAlertType.error,
+        text: errore,
+        title: "Attenzione"
+    );
+  }
+
+  Future<List<OrdinazioneData>> setDefault() async {
+    StatisticheControl db = StatisticheControl();
+    return StatisticheControl().getGuadagniTotaliFromDB();
+  }
+
 }
